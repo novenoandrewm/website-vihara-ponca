aa1eba72921836df860af2cc9bf86e998e1a1b3b
Nv <135493024+endvvvv@users.noreply.github.com>
2025-12-17 23:01:59 +0000

    test: load complete i18n messages to avoid missing-key warnings

--- tests/setup.ts
+++ tests/setup.ts
@@ -2,18 +2,20 @@ import { config } from '@vue/test-utils'
 import { createI18n } from 'vue-i18n'
 import 'vitest-axe/extend-expect'
 
+import id from '@/i18n/id.json'
+import en from '@/i18n/en.json' // kalau file ini ada
+
 Object.defineProperty(window.HTMLCanvasElement.prototype, 'getContext', {
   value: () => null,
 })
+
 const i18n = createI18n({
   legacy: false,
   locale: 'id',
   fallbackLocale: 'id',
-  messages: {
-    id: { nav: { home: 'Beranda', pmv: 'PMV', gabi: 'GABI' } },
-    en: { nav: { home: 'Home', pmv: 'PMV', gabi: 'GABI' } },
-  },
+  messages: { id, en },
 })
+
 config.global.plugins = [i18n]
 config.global.stubs = {
   RouterLink: { props: ['to'], template: `<a :href="to"><slot /></a>` },
